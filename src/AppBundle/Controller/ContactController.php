8a4411dd146c5c7e2c43be108d4cde6b1136396f
Steve Popoola <stevepop@gmail.com>
2016-04-27 09:45:36 +0100

    Display contacts list and contact view page

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -16,13 +17,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class ContactController extends Controller
 {
 	/**
-	 * @Route("/contact", name="contacts")
+	 * @Route("/contact/{id}", name="contact_page", requirements={"id": "\d+"})
  	 * @Method({"GET"})
 	 */
-    public function showAction(Request $request)
+    public function showAction($id)
     {
+        // Get the Doctrine entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        $contact = $em->getRepository(Contact::class)->find($id);
+
         return $this->render(
-            'contact/show.html.twig'
+            'contact/show.html.twig', ['contact' => $contact]
         );
     }
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 
 class DefaultController extends Controller
@@ -11,8 +15,13 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        // Get the Doctrine entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        $contacts = $em->getRepository(Contact::class)->findAll();
+
+        return $this->render('default/index.html.twig', ['contacts' => $contacts]);
     }
 }
